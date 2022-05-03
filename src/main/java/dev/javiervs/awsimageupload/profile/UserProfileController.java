d5d99336d9f7b60cbd8e13284bc5d42803596a92
javiervs.dev <javiervs.dev@gmail.com>
2022-05-03 17:19:12 +0200

    Created upload image endpoint

--- src/main/java/dev/javiervs/awsimageupload/profile/UserProfileController.java
+++ src/main/java/dev/javiervs/awsimageupload/profile/UserProfileController.java
@@ -1,11 +1,13 @@
 package dev.javiervs.awsimageupload.profile;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.awt.*;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,5 +19,15 @@ public class UserProfileController {
     @GetMapping
     public List<UserProfile> getUserProfiles() {
         return userProfileService.getUserProfiles();
+    }
+
+    @PostMapping(
+            path = "{userProfileId}/image/upload",
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public void uploadUserProfileImage(@PathVariable UUID userProfileId,
+                                       @RequestParam MultipartFile file) {
+        userProfileService.uploadUserProfileImage(userProfileId, file);
     }
 }
--- src/main/java/dev/javiervs/awsimageupload/profile/UserProfileService.java
+++ src/main/java/dev/javiervs/awsimageupload/profile/UserProfileService.java
@@ -2,13 +2,19 @@ package dev.javiervs.awsimageupload.profile;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public record UserProfileService(UserProfileRepository userProfileRepository) {
 
     public List<UserProfile> getUserProfiles() {
         return userProfileRepository.getUserProfiles();
+    }
+
+    public void uploadUserProfileImage(UUID userProfileId, MultipartFile file) {
+
     }
 }
